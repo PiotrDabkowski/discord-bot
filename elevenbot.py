52e78f74b52eba650814d136ed1a883a290f4317
ColdCawfee <79474598+ColdCawfee@users.noreply.github.com>
2023-02-23 21:26:45 -0400

    removed 'about' cmd

--- elevenbot.py
+++ elevenbot.py
@@ -238,23 +238,6 @@ async def voicelistcmd(ctx: lightbulb.context.Context):
         os.remove("voices.json")
 
 @bot.command
-@lightbulb.command("about", "Details about the bot.")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def aboutcmd(ctx: lightbulb.context.Context):
-    randcolor = "#" + ''.join([random.choice('ABCDEF0123456789') for i in range(6)])
-    embed = hikari.Embed(
-        title="About",
-        description="About this bot.",
-        color=randcolor
-    ).add_field(
-        "About.",
-        "This bot is an unofficial bot for ElevenLabs, an AI TTS website where you can make your own voices.\n\nThere are 3 commands for this bot: 'synthesize', 'login', and 'voices'.\n\nThe login command is required to use the bot. If you do not run this command first, you will not be able to use the bot to it's full extent. **Nothing you enter will be saved anywhere.** \n\nThe 'synthesize' command will let you use the voices, but only if you are logged in.\n\nThe 'voices' command will let you view all voices on your account, but just like the synthesize command, will only work if you are logged in."
-    ).set_footer(
-        "This bot was made by @HawtCawfee☕#1337 in collaboration with ElevenLabs. If any issues arise, please contact @HawtCawfee☕#1337 first before contacting ElevenLabs."
-    ).set_thumbnail("https://i.ibb.co/Gn6C0s5/1668126895443.jpg")
-    await ctx.respond(embed)
-
-@bot.command
 @lightbulb.command("login", "Login using your API key.")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def logincmd(ctx: lightbulb.context.Context):
