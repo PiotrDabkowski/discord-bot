a0c6563f2cffc484cf80774cae00bff3500f1984
ColdCawfee <79474598+ColdCawfee@users.noreply.github.com>
2023-04-03 17:44:27 -0300

    Update elevenbot.py
    removed obsolete cmds, fixed logging in, added deprecate warning

--- elevenbot.py
+++ elevenbot.py
@@ -1,11 +1,3 @@
-import hikari, lightbulb, requests, random, os, time, json as j, miru, asyncio
-from loginModal import ModalView
-from suggestModal import ModalView as modalsuggest
-
-
-bot = lightbulb.BotApp("")
-miru.install(bot)
-
 @bot.listen(hikari.StartedEvent)
 async def on_ready(event):
     print("Ready!")
@@ -74,7 +66,7 @@ async def pingcmd(ctx: lightbulb.context.Context):
     return
 
 @bot.command
-@lightbulb.option("use_stream", "Whether or not to use the audio stream method, which might be faster, but could break at any time.", required=False, type=bool, default=False)
+@lightbulb.option("use_stream", "Whether or not to use the audio stream method, which might be faster, but could break at any time.", required=True, type=bool)
 @lightbulb.option("text", "The text to synthesize.")
 @lightbulb.command("favorite-synthesize", "Synthesize using favorite voice ID.")
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -138,41 +130,6 @@ async def logoutcmd(ctx: lightbulb.context.Context):
         await ctx.edit_last_response("Key deleted! ✅")
 
 @bot.command
-@lightbulb.option("similarity", "New similarity amount. Enter a value between 0 and 1.", type=float, required=True)
-@lightbulb.option("stability", "New stability amount. Enter a value between 0 and 1.", type=float, required=True)
-@lightbulb.option("voice_id", "The voice ID of the voice you want to change.")
-@lightbulb.command("change-voice-settings", "Change settings.")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def changesettingscmd(ctx: lightbulb.context.Context):
-    if not os.path.exists(f"{ctx.user.id}.txt"):
-        await ctx.respond("You are not logged in! Please run '/login' to continue.")
-        return
-    else:
-        site = "https://api.elevenlabs.io/v1/voices/" + ctx.options.voice_id + "/settings/edit"
-        with open(f"{ctx.user.id}.txt", 'r') as outfile:
-            headers = {
-                'accept': 'application/json',
-                'xi-api-key': outfile.read(),
-                'Content-Type': 'application/json'
-            }
-            outfile.close()
-        if ctx.options.stability >= 2:
-            await ctx.respond("Stability is too high! Please lower value.")
-            return
-        elif ctx.options.similarity >= 2:
-            await ctx.respond("Similarity is too high! Please lower value.")
-            return
-        else:
-            await ctx.respond("Changing voice settings... ⏰")
-            r = requests.post(site, json={"stability": ctx.options.stability, "similarity_boost": ctx.options.similarity}, headers=headers)
-            if r.status_code == 200:
-                await ctx.edit_last_response("Done ✅! New settings should now be set.")
-                return
-            elif r.status_code == 500:
-                await ctx.edit_last_response("An error happened! Try inputting a lower number.")
-                return
-
-@bot.command
 @lightbulb.option("voice_id", "The voice ID you want to add to the favorites.")
 @lightbulb.command("set-favorite", "Set a voice ID to favorites. Run '/favorite-synthesize' to use your favorite voice ID.")
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -200,26 +157,6 @@ async def removefavcmd(ctx: lightbulb.context.Context):
         return    
 
 @bot.command
-@lightbulb.option("voice_id", "Voice ID to get settings.", required=True)
-@lightbulb.command("get-voice-settings", "Gets the settings for the voice.")
-@lightbulb.implements(lightbulb.SlashCommand)
-async def getcurrentsettingscmd(ctx: lightbulb.context.Context):
-    if not os.path.exists(f"{ctx.user.id}.txt"):
-        await ctx.respond("You are not logged in! Please run '/login' to continue.")
-        return
-    else:
-        site = "https://api.elevenlabs.io/v1/voices/" + ctx.options.voice_id + "/settings"
-        with open(f"{ctx.user.id}.txt", 'r') as outfile:
-            headers = {
-                'accept': 'application/json',
-                'xi-api-key': outfile.read()
-            }
-            outfile.close()
-        await ctx.respond("Getting settings... ⏰")
-        r = requests.get(site, headers=headers)
-        await ctx.edit_last_response(f"Stability: {r.json()['stability']}\nSimilarity: {r.json()['similarity_boost']}")
-
-@bot.command
 @lightbulb.command("userinfo", "Gets more info about character count, sub tier, etc")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def userinfocmd(ctx: lightbulb.context.Context):
@@ -238,7 +175,6 @@ async def userinfocmd(ctx: lightbulb.context.Context):
         r = requests.get(site, headers=headers)
         await ctx.edit_last_response(f"User info for user {ctx.user.username}:\n\nSubscription: {r.json()['subscription']['tier']}\nCharacter count: {r.json()['subscription']['character_count']}\nCharacter limit: {r.json()['subscription']['character_limit']}\nCan user extend character limit? {r.json()['subscription']['can_extend_character_limit']}\nIs user allowed to extend character limit? {r.json()['subscription']['allowed_to_extend_character_limit']}\nTime until next character reset (in unix): {r.json()['subscription']['next_character_count_reset_unix']}\nVoice limit: {r.json()['subscription']['voice_limit']}\nCan user extend voice limit? {r.json()['subscription']['can_extend_voice_limit']}\nCan user use instant voice cloning? {r.json()['subscription']['can_use_instant_voice_cloning']}\nIs user a new user? {r.json()['is_new_user']}")
         
-
 @bot.command
 @lightbulb.command("voices", "Gets a list of all voices that are on your account.")
 @lightbulb.implements(lightbulb.SlashCommand)
@@ -268,9 +204,13 @@ async def voicelistcmd(ctx: lightbulb.context.Context):
 @lightbulb.command("login", "Login using your API key.")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def logincmd(ctx: lightbulb.context.Context):
-    view = ModalView()
-    message = await ctx.respond("Click the button below to sign in.\n\n**This info will have to be saved but will only be used to validate your profile.**", components=view)
-    await view.start(message)
+    if os.path.exists(f"{ctx.user.id}.txt"):
+        await ctx.respond("No need to login again! You are already logged in.")
+        return
+    else:
+        view = ModalView()
+        message = await ctx.respond("Click the button below to sign in.\n\n**This info will have to be saved but will only be used to validate your profile.**", components=view)
+        await view.start(message)
 
 bot.run(
     activity=hikari.Activity(
